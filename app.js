0a375e9147207f02d8fdea2a4d992321e9731829
Turki767 <62534076+TurkiPro@users.noreply.github.com>
2022-07-10 05:02:24 +0300

    setting up express and mongoose

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const exports = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
@@ -6,3 +6,17 @@ mongoose.connect('mongodb://localhost:27017/ToDo', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+const schema = new mongoose.Schema({ title: String });
+const Task = mongoose.model('Task', schema);
+
+
+//insert
+app.get('/create', (req, res) => {
+    const fisrtTask = new Task({ title: 'writing insert function' });
+    fisrtTask.save().then(() => console.log('new record inserted'));
+})
+
+app.listen(3000, () => console.log('express started on port 3000'));
+
+
