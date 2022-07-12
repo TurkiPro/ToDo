3d7aaf6708c45457e9a64c8799d6ce1f69ede36b
Turki767 <62534076+TurkiPro@users.noreply.github.com>
2022-07-13 00:00:45 +0300

    edited previous functions and added an update functions

--- app.js
+++ app.js
@@ -12,10 +12,43 @@ const Task = mongoose.model('Task', schema);
 
 
 //insert
-app.get('/create', (req, res) => {
-    const fisrtTask = new Task({ title: 'writing insert function' });
+app.get('/create/:title', (req, res) => {
+    const fisrtTask = new Task({ title: req.params.title });
     fisrtTask.save().then(() => console.log('new record inserted'));
-})
+});
+
+//find
+app.get('/', (req, res) => {
+    Task.find({}, (error, tasks) => {
+        if (error) console.log('there was an error: ${error}');
+        else {
+            tasks.forEach(task => console.log(task));
+        }
+
+    });
+});
+
+//delete
+app.get('/delete/:id', (req, res) => {
+    Task.deleteOne({ _id: req.params.id }, (error) => {
+        if (error) console.log('there was an error: ${error}');
+        else {
+            console.log('one Task is deleted');
+        }
+    });
+});
+
+//update
+app.get('/update/:id/:title', (req, res) => {
+    Task.updateOne({ _id: req.params.id }, { title: req.params.title },
+        (error) => {
+            if (error) console.log('there was an error: ${error}');
+            else {
+                console.log('one Task is updated');
+            }
+        });
+});
+
 
 app.listen(3000, () => console.log('express started on port 3000'));
 
